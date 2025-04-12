1e2ce70fd402196c24c9744f8439178cc4edbd9b
Wataru Matsuyama <wataruxun@gmail.com>
2025-04-12 15:37:48 +0900

    [add] 打刻履歴取得APIと打刻APIを追加

--- mock-server/src/index.ts
+++ mock-server/src/index.ts
@@ -21,4 +21,56 @@ app.use('*', cors({
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }))
 
+// 打刻履歴取得API
+app.get('/attendances/history', (c) => {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const dayBeforeYesterday = new Date(today);
+  dayBeforeYesterday.setDate(today.getDate() - 2);
+  return c.json([
+    {
+      date: dayBeforeYesterday.toISOString().split('T')[0],
+      startTime: '08:30',
+      endTime: '17:30',
+      breaks: [
+        {
+          start: '12:00',
+          end: '13:00'
+        }
+      ]
+    },
+    {
+      date: yesterday.toISOString().split('T')[0],
+      startTime: '09:00',
+      endTime: '18:00',
+      breaks: [
+        {
+          start: '12:00',
+          end: '13:00'
+        }
+      ]
+    },])
+})
+
+// 打刻API
+app.post('/attendances', async (c) => {
+  const { startTime, endTime, breaks } = await c.req.json();
+  const today = new Date();
+  const date = today.toISOString().split('T')[0];
+  const start = startTime || today.toTimeString().split(' ')[0].slice(0, 5);
+  const end = endTime || today.toTimeString().split(' ')[0].slice(0, 5);
+  const breakTimes = breaks || [];
+  const breakTimesFormatted = breakTimes.map((b: { start: string; end: string }) => ({
+    start: b.start || today.toTimeString().split(' ')[0].slice(0, 5),
+    end: b.end || today.toTimeString().split(' ')[0].slice(0, 5)
+  }));
+  return c.json({
+    date,
+    startTime: start,
+    endTime: end,
+    breaks: breakTimesFormatted
+  });
+})
+
 app.route('/', router);
